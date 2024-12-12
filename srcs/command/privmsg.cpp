2c169a0a011d2a466dd6b8ff38b34986fe3090a4
pokotieru-mashumaro <k827umeboshi@gmail.com>
2024-12-12 12:44:15 +0900

    asdf

--- srcs/command/privmsg.cpp
+++ srcs/command/privmsg.cpp
@@ -30,6 +30,8 @@ void Client::privmsg(Server *server, Client *client, std::string param)
         return server->SendMsg2Client(client->getFd(), SYNTAX_ERROR(client->getNickName(), "PRIVMSG"));
     if (!is_ok_target(server, params[0]))
         return server->SendMsg2Client(client->getFd(), ERROR_401(client->getNickName(), params[0]));
+    if (params[1] != "" && params[1][0] == ':')
+        params[1].erase(0, 1);
     if (params[0][0] == '#')
     {
         Channel *channel = server->getChannel(params[0]);
